df912f7512a2ad1a7c5dddf50ba1c3d071b56c63
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2022-12-13 11:49:30 -0300

    [Stream App] Refactor and create custom chip

--- lab/stream_app/libraries/vhs/lib/src/widgets/filter_chips/filter_chips_widget.dart
+++ lab/stream_app/libraries/vhs/lib/src/widgets/filter_chips/filter_chips_widget.dart
@@ -22,8 +22,8 @@ class _FilterChipsWidgetState extends State<FilterChipsWidget> {
         itemBuilder: ((context, index) {
           return Padding(
             padding: const EdgeInsets.only(right: 12.0),
-            child: FilterChip(
-              label: Text(widget.filters[index]),
+            child: VhsSelectableChip(
+              label: widget.filters[index],
               selectedColor: const Color(0xff69FF9B),
               backgroundColor: const Color(0xff2A2939),
               selected: index.isOdd,
@@ -31,6 +31,52 @@ class _FilterChipsWidgetState extends State<FilterChipsWidget> {
             ),
           );
         }),
+      ),
+    );
+  }
+}
+
+class VhsSelectableChip extends StatelessWidget {
+  final String label;
+  final Color selectedColor;
+  final Color backgroundColor;
+  final bool selected;
+  final void Function(String value) onSelected;
+
+  const VhsSelectableChip({
+    super.key,
+    required this.label,
+    required this.selectedColor,
+    required this.backgroundColor,
+    required this.selected,
+    required this.onSelected,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        padding: const EdgeInsets.only(top: 6, right: 14, bottom: 6, left: 12),
+        decoration: BoxDecoration(
+          color: selected ? selectedColor : backgroundColor,
+          borderRadius: BorderRadius.circular(100),
+        ),
+        child: Row(
+          children: [
+            if (selected)
+              const Icon(
+                Icons.check,
+                size: 16,
+              ),
+            Text(
+              label,
+              style: TextStyle(
+                color: selected ? backgroundColor : Colors.white,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
